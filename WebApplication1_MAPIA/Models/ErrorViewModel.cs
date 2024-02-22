7f0e5479fca186b5cc32410a5cc8d3b726876bb7
55169 <gabriel.okada14@gmail.com>
2024-02-22 12:30:11 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1_MAPIA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_MAPIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
